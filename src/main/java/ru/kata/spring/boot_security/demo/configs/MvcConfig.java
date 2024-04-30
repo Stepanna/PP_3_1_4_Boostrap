36a1083937a383d7302983bcd6a9b59c01d63e57
Stepchenko Anna <43602868+Stepanna@users.noreply.github.com>
2024-04-30 15:50:37 +0300

    PP_3_1_5 Rest controllers

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -1,12 +1,15 @@
 package ru.kata.spring.boot_security.demo.configs;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
+@Component
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/user").setViewName("user_page");
+        registry.addViewController("/admin").setViewName("admin");
     }
 }
